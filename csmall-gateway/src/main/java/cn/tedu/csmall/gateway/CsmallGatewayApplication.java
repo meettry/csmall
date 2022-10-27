ccb0925c91d5bed766965873c80e54558105c7c9
meettry <1326508505@qq.com>
2022-10-27 16:22:02 +0800

    10.27更新:网关

---
+++ csmall-gateway/src/main/java/cn/tedu/csmall/gateway/CsmallGatewayApplication.java
@@ -0,0 +1,13 @@
+package cn.tedu.csmall.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsmallGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CsmallGatewayApplication.class, args);
+    }
+
+}
