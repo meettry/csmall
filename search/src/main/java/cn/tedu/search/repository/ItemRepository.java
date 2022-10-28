7820590fdbc4c56f3c80fcbe562ff78b1aa4d323
meettry <1326508505@qq.com>
2022-10-28 16:26:20 +0800

    10.27更新:ES搜索

--- search/src/main/java/cn/tedu/search/repository/ItemRepository.java
+++ search/src/main/java/cn/tedu/search/repository/ItemRepository.java
@@ -1,6 +1,8 @@
 package cn.tedu.search.repository;
 
 import cn.tedu.search.entity.Item;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 import org.springframework.stereotype.Repository;
 
@@ -17,4 +19,30 @@ public interface ItemRepository extends ElasticsearchRepository<Item,Long> {
     // 会对这个索引自动生成基本的增删改查方法,我们无需再编写
     // ElasticsearchRepository<[要操作的\关联的实体类名称],[实体类主键的类型]>
 
+    // SpringData实现自定义查询
+    // 我们要编写遵循SpringData给定格式的方法名
+    // SpringBoot会根据方法名,自动推断出查询目的,生成查询语句完成操作
+    // query(查询):表达当前方法是一个查询方法,等价于select
+    // Item,Items:确定要表达的实体类(对应的索引),不带s是查询单个对象的,带s的查集合
+    // By(通过/根据),标识开始设置查询的单词,等于sql中的where
+    // title:要查询的字段,可以使Item实体类中声明的任何字段
+    // Matches(匹配):执行查询条件的操作,Matches是匹配字符串类型的关键字,支持分词等价的sql中的like
+    Iterable<Item> queryItemsByTitleMatches(String title);
+
+    // 多条件查询
+    // 多个条件之间要用and或or来分隔,表示多个条件间的逻辑关系
+    // 假设需要Title和Brand进行多条件查询
+    // 多个条件时,方法名就要按规则编写多个条件,参数也要对应条件的个数
+    // 声明的参数会按照顺序赋值给方法名中的条件,和参数无关
+    Iterable<Item> queryItemsByTitleMatchesAndBrandMatches(String a,String b);
+
+    // 排序查询
+    Iterable<Item> queryItemsByTitleMatchesOrBrandMatchesOrderByPriceDesc(String title,String brand);
+
+    // 分页查询
+    // 返回值类型需要修改为Page类型,这个类型既可以保存从ES中查询到的数据,
+    // 又可以保存当前分页查询的分页信息: 当前页,总页数,总条数,每页条数,有没有上一页,下一页等
+    // 分页查询方法要添加一个Pageable,必须放在现有所有参数的后面
+    // 它可以设置要查询的页码和每页的条数
+    Page<Item> queryItemsByTitleMatchesOrBrandMatchesOrderByPriceDesc(String title, String brand, Pageable pageable);
 }
--- search/src/test/java/cn/tedu/search/SearchApplicationTests.java
+++ search/src/test/java/cn/tedu/search/SearchApplicationTests.java
@@ -6,7 +6,11 @@ import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 @Slf4j
@@ -31,7 +35,7 @@ class SearchApplicationTests {
         log.debug("ok");
     }
 
-    //单查
+    // 单查
     @Test
     void getOne(){
         // SpringDataElasticsearch提供了按id查询数据的方法
@@ -40,7 +44,82 @@ class SearchApplicationTests {
         Optional<Item> optional = itemRepository.findById(1L);
         Item item = optional.get();
         System.out.println("item="+item);
+        log.debug("abcabc");
+    }
 
+    // 批量增
+    @Test
+    void addList(){
+        // 实例化一个List对象,用户添加要保存到ES的Item对象
+        List<Item> list = new ArrayList<>();
+        list.add(new Item(2L,"罗技激光有线办公鼠标","鼠标","罗技",9.9,"/2.jpg"));
+        list.add(new Item(3L,"雷蛇机械无线游戏键盘","键盘","雷蛇",262.0,"/3.jpg"));
+        list.add(new Item(4L,"微软有线静音办公鼠标","鼠标","微软",190.0,"/4.jpg"));
+        list.add(new Item(5L,"罗技机械有线背光键盘","键盘","罗技",236.0,"/5.jpg"));
+        itemRepository.saveAll(list);
+        log.debug("插入批量数据成功,参数:{}",list);
+    }
 
+    // 全查
+    @Test
+    void getAll(){
+
+        Iterable<Item> items = itemRepository.findAll();
+        for (Item item : items) {
+            System.out.println(item);
+
+        }
+    }
+
+    // 单条件自定义查询
+    @Test
+    void queryOne(){
+        // 查询ES中items索引数据,title字段包含"游戏"分词数据
+        Iterable<Item> items = itemRepository.queryItemsByTitleMatches("有线游戏");
+        for (Item item : items) {
+            log.debug("item:{}",item);
+
+        }
+    }
+
+    // 多条件自定义查询
+    @Test
+    void queryTwo(){
+        // 查询ES中,items索引里,title字段包含"游戏",并且品牌是"罗技"的数据
+        Iterable<Item> items = itemRepository.queryItemsByTitleMatchesAndBrandMatches("有线游戏","罗技");
+        for (Item item : items) {
+            log.debug("item:{}",item);
+
+        }
+    }
+
+    // 自定义排序查询
+    @Test
+    void queryOrder(){
+        // 查询ES中,items索引里,title字段包含"游戏",并且品牌是"罗技"的数据
+        Iterable<Item> items = itemRepository.queryItemsByTitleMatchesOrBrandMatchesOrderByPriceDesc("游戏","罗技");
+        for (Item item : items) {
+            log.debug("item:{}",item);
+        }
+    }
+
+    // 自定义分页查询
+    @Test
+    void queryPage(){
+        int page = 1;       // 要查询的页码
+        int pageSize=2;     // 每页条数设置
+        // PageRequest.of(int page,int pageSize) 其中 page参数表示要查询的页码,0表示第一页,以此类推
+        Page<Item> pages = itemRepository.queryItemsByTitleMatchesOrBrandMatchesOrderByPriceDesc("游戏", "罗技", PageRequest.of(page-1, pageSize));
+        log.debug(""+pages);
+        for (Item item : pages) {
+            log.debug("item:{}",item);
+        }
+        // pages对象包含的分页信息输出
+        log.debug("总页数:{}",pages.getTotalPages());
+        log.debug("总条数:{}",pages.getTotalElements());
+        log.debug("当前页:{}",pages.getNumber()+1); // pages.getNumber()返回0时,表示第一页
+        log.debug("每页条数:{}",pages.getSize());
+        log.debug("是否是首页:{}",pages.isFirst());
+        log.debug("是否是末页:{}",pages.isLast());
     }
 }
